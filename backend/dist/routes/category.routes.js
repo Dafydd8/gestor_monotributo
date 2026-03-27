fc05067eaddea0955f2ccbccedd1884b249a28a5
Dafydd8 <dafydd.a.jenkins@gmail.com>
2026-03-27 17:02:57 -0300

    cron job

--- backend/dist/routes/category.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const category_controller_1 = require("../controllers/category.controller");
-const router = (0, express_1.Router)();
-router.get("/current", category_controller_1.getCurrent);
-router.get("/overview", auth_middleware_1.authMiddleware, category_controller_1.getOverview);
-exports.default = router;
