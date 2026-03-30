aeab76a64ecc547316d98de5aab5d74b35807718
Dafydd8 <dafydd.a.jenkins@gmail.com>
2026-03-30 14:46:26 -0300

    changes to prisma

--- backend/src/scripts/syncCategories.ts
+++ backend/src/scripts/syncCategories.ts
@@ -1,14 +1,9 @@
 import "dotenv/config";
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 const SOURCE_URL = "https://www.afip.gob.ar/monotributo/categorias.asp";
 
